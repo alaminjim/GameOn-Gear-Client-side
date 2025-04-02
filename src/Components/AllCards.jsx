79121ae347b101c3acde9fac998e4e33d358cc92
alaminjim <jimalamin7@gmail.com>
2025-04-02 11:53:30 +0800

    all cards

--- src/Components/AllCards.jsx
+++ src/Components/AllCards.jsx
@@ -11,7 +11,7 @@ const AllCards = ({ item }) => {
       <div className="bg-[#b4be2d43]">
         <div className="flex">
           <img src={image} className="max-w-sm rounded-lg m-5 shadow-xl" />
-          <div className="pt-8 space-y-2.5">
+          <div className="pt-8 space-y-2">
             <h2 className="card-title font-bold text-2xl text-[#B5BE2D]">
               {itemName}
             </h2>
@@ -37,8 +37,12 @@ const AllCards = ({ item }) => {
               {rating}
             </p>
             <div className="space-x-3">
-              <button className="btn btn-primary">Update</button>
-              <button className="btn btn-primary">Delete</button>
+              <button className="w-[95px]  h-10 text-[#B5BE2D] font-bold border-2 border-[#B5BE2D] hover:bg-[#B5BE2D] hover:text-white rounded-full">
+                Update
+              </button>
+              <button className="w-[95px]  h-10 text-red-300 font-bold border-2 border-red-300 hover:bg-red-300 hover:text-white rounded-full">
+                Delete
+              </button>
             </div>
           </div>
         </div>
